37f76e3288171afc54e95909a501a49d9964cf9a
thegrinder <paul.szczepkowski@gmail.com>
2019-10-19 21:08:04 +0200

    feat: add custom absolute-fill class name

--- plugins/custom.js
+++ plugins/custom.js
@@ -24,6 +24,13 @@ const customPlugin = ({ addUtilities }) => {
       overflow: 'hidden',
       maxWidth: '100%',
     },
+    '.absolute-fill': {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0
+    }
   }];
   addUtilities(utilities);
 };
